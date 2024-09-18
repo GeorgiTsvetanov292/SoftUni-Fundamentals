dbebbc586b793b6f84be0f7c1e2af59ad70d29ae
GeorgiTsvetanov292 <gogicha292@gmail.com>
2024-09-18 18:22:23 +0300

    Delete 1.StudentInformation directory

--- 1.StudentInformation/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿string name = Console.ReadLine();
-int age = int.Parse(Console.ReadLine());
-double averageGrade = double.Parse(Console.ReadLine());
-
-Console.WriteLine($"Name: {name}, Age: {age}, Grade: {averageGrade:f2}");
