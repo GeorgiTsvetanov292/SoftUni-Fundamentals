a840187a07c2634eacbd87d334f3179043319597
GeorgiTsvetanov292 <gogicha292@gmail.com>
2024-09-20 00:46:25 +0300

    Add files via upload

---
+++ 04.BackIn30Minutes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Channels;
+
+int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+minutes += 30;
+
+if (minutes >= 60)
+{
+    minutes-= 60;
+    hours++;
+}
+if (hours == 24)
+{
+    hours = 0;
+}
+
+if (minutes < 10)
+{
+    Console.WriteLine($"{hours}:0{minutes}");
+}
+else
+{
+    Console.WriteLine($"{hours}:{minutes}");
+
+}
+
