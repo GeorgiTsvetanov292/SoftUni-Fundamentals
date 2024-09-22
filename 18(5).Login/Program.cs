ba64769c7702a54d3c417855583b4d14b8f3e641
GeorgiTsvetanov292 <gogicha292@gmail.com>
2024-09-23 02:57:03 +0300

    Add files via upload

---
+++ 18(5).Login/Program.cs
@@ -0,0 +1,31 @@
+﻿string username = Console.ReadLine();
+string user = username;
+
+char[] stringArray = username.ToCharArray();
+Array.Reverse(stringArray);
+string password = new string(stringArray);
+
+int fails = 0;
+
+while (true)
+{
+    username = Console.ReadLine();
+
+
+    if (username == password)
+    {
+        Console.WriteLine($"User {user} logged in.");
+        break;
+    }
+    fails++;
+
+    if (fails == 4)
+    {
+        Console.WriteLine($"User {user} blocked!");
+        break;
+    }
+    if (username != password)
+    {
+        Console.WriteLine($"Incorrect password. Try again.");
+    }
+}
---
+++ 19(6).StrongNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int copy = number;
+int factorialSum = 0;
+
+while (copy > 0)
+{
+    int digit = copy % 10;
+    copy /= 10;
+
+
+    int sum = 1;
+    for (int i = 1; i <= digit; i++)
+    {
+        sum *= i;
+    }
+    factorialSum += sum;
+
+}
+bool isStrong = factorialSum == number;
+Console.WriteLine(isStrong ? "yes" : "no");
+
+
