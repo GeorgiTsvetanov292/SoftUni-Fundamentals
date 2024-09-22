56bb3188baee39ca6ed5e24d19eeceaed688eaed
GeorgiTsvetanov292 <gogicha292@gmail.com>
2024-09-23 02:59:47 +0300

    Add files via upload

---
+++ 21(8).TriangleOfNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int maxRows = int.Parse(Console.ReadLine());
+
+for (int row = 1; row <= maxRows; row++)
+{
+    for (int col = 0; col < row; col++)
+    {
+        Console.Write($"{row} ");
+
+    }
+    Console.WriteLine();
+}
