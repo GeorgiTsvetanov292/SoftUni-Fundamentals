38066e9f7bb7ef6db8d36cfba80767db22de2c37
GeorgiTsvetanov292 <gogicha292@gmail.com>
2024-09-24 00:08:06 +0300

    Add files via upload

---
+++ 22(9).PadawanEquipment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Linq;
+
+double budget = double.Parse(Console.ReadLine());
+int students = int.Parse(Console.ReadLine());
+double saberPrice = double.Parse(Console.ReadLine());
+double robePrice = double.Parse(Console.ReadLine());
+double beltPrice = double.Parse(Console.ReadLine());
+
+double percent = students * 10.0 / 100;
+double allSabers = students + Math.Ceiling(percent);
+double sabersPrice = saberPrice * allSabers;
+
+double robesPrice = robePrice * students;
+
+double beltsDiscount = students - Math.Ceiling((double)(students / 6));
+double beltsPrice = beltPrice * beltsDiscount;
+
+
+double total = sabersPrice + robesPrice + beltsPrice;
+
+if (total <= budget)
+{
+    Console.WriteLine($"The money is enough - it would cost {total:f2}lv.");
+}
+else
+{
+    Console.WriteLine($"John will need {total - budget:f2}lv more.");
+}
---
+++ 23(10).RageExpenses/Program.cs
@@ -0,0 +1,32 @@
+﻿int gamesLost = int.Parse(Console.ReadLine());
+double headsetPrice = double.Parse(Console.ReadLine());
+double mousePrice = double.Parse(Console.ReadLine());
+double keyboardPrice = double.Parse(Console.ReadLine());
+double displayPrice = double.Parse(Console.ReadLine());
+
+double expenses = 0;
+int keyboardBroken = 0;
+
+for (int i = 1; i <= gamesLost; i++)
+{
+    if (i % 2 == 0)
+    {
+        expenses += headsetPrice;     
+    }
+    if (i % 3 == 0)
+    {
+        expenses += mousePrice;     
+    }
+    if (i % 2 == 0 && i % 3 == 0)
+    {
+        expenses += keyboardPrice;
+        keyboardBroken++;
+    }
+    if (keyboardBroken == 2)
+    {
+        expenses += displayPrice;
+        keyboardBroken = 0;
+    }
+
+}
+Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
