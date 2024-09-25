7f8ce72133c2a19cedaaaa3ea8d052346b7888fb
GeorgiTsvetanov292 <gogicha292@gmail.com>
2024-09-26 01:41:36 +0300

    Add files via upload

---
+++ 29(5).Messages/Program.cs
@@ -0,0 +1,24 @@
+﻿int numbers = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numbers; i++)
+{
+    string currentNumber = Console.ReadLine();
+    int numberOfDigits = currentNumber.Length;
+
+
+    int mainDigit = int.Parse(currentNumber) % 10;
+    if (mainDigit == 0)
+    {
+        Console.Write(" ");
+        continue;
+    }
+    int offSet = (mainDigit - 2) * 3;
+
+    if (mainDigit == 8 || mainDigit == 9)
+    {
+        offSet++;
+    }
+    int letterIndex = (offSet + numberOfDigits - 1);
+    char finalLetter = (char)((int)'a' + letterIndex);
+    Console.Write(finalLetter);
+}
