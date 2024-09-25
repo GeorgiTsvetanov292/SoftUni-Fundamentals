591042240ff13019cb75eac5bade27731bd7cba9
GeorgiTsvetanov292 <gogicha292@gmail.com>
2024-09-25 23:51:20 +0300

    Add files via upload

---
+++ 24(11).Orders/Program.cs
@@ -0,0 +1,16 @@
+﻿int orders = int.Parse(Console.ReadLine());
+
+double total = 0;
+
+for (int i = 0; i < orders; i++) 
+{
+    double capsulePrice = double.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    int capsulesCount = int.Parse(Console.ReadLine());
+
+    double currentOrder = (days * capsulesCount) * capsulePrice;
+    total += currentOrder;
+
+    Console.WriteLine($"The price for the coffee is: ${currentOrder:f2}");
+}
+Console.WriteLine($"Total: ${total:f2}");
